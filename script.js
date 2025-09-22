f80672bb53e0463c4a7945eadfcc9d2cdd05daea
Akki789 <151871443+Akki789@users.noreply.github.com>
2025-09-22 11:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const squares = document.querySelectorAll('.square');
 
 squares.forEach((square, index) => {
-  square.addEventListener('mouseenter', () => {
+  square.addEventListener('mouseover', () => {
     squares.forEach((otherSquare, i) => {
       if (i !== index) {
         otherSquare.style.backgroundColor = '#6F4E37'; // Coffee
@@ -10,7 +10,7 @@ squares.forEach((square, index) => {
     });
   });
 
-  square.addEventListener('mouseleave', () => {
+  square.addEventListener('mouseout', () => {
     squares.forEach(otherSquare => {
       otherSquare.style.backgroundColor = '#E6E6FA'; // Lavender
     });
